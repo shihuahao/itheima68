5d05fed7cbd42025a05d7bda5218bae1cafc6eca
shihuahao <shihuahao0353@163.com>
2018-01-04 09:59:35 +0800

    e3-portal-web

--- e3-portal-web/src/main/webapp/js/e3mall.js
+++ e3-portal-web/src/main/webapp/js/e3mall.js
@@ -1,17 +1,20 @@
-var TT = TAOTAO = {
+var E3 = E3MALL = {
 	checkLogin : function(){
-		var _ticket = $.cookie("TT_TOKEN");
+		//获取cookie中token
+		var _ticket = $.cookie("E3_TOKEN");
 		if(!_ticket){
 			return ;
 		}
+		//向单点登陆发请求，获取用户身份信息
 		$.ajax({
-			url : "http://localhost:8084/user/token/" + _ticket,
+			url : "http://localhost:8089/user/token/" + _ticket,
 			dataType : "jsonp",
 			type : "GET",
 			success : function(data){
 				if(data.status == 200){
 					var username = data.data.username;
-					var html = username + "，欢迎来到淘淘！<a href=\"http://www.taotao.com/user/logout.html\" class=\"link-logout\">[退出]</a>";
+					var html = username + "，欢迎来到宜立方！<a href=\"http://localhost:8089/user/logout/"+_ticket+"\" class=\"link-logout\">[退出]</a>";
+					//页面回显
 					$("#loginbar").html(html);
 				}
 			}
@@ -21,5 +24,5 @@ var TT = TAOTAO = {
 
 $(function(){
 	// 查看是否已经登录，如果已经登录查询登录信息
-	TT.checkLogin();
+	E3.checkLogin();
 });
